ec48f594ac7047c152cd4c5ddd4e843e147c8b1d
Erik Shaiken <erik.shaiken01@gmail.com>
2022-11-06 18:02:00 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React from "react";
+import First_section from "./components/First_section";
+const App = () => {
+  return (
+    <div className="App">
+      <First_section />
+      <section className="second_section"></section>
+      <section className="third_section"></section>
+      <section className="fourth_section"></section>
+      <section className="prefooter"></section>
+      <footer></footer>
+    </div>
+  );
+};
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ src/components/First_section.jsx
@@ -0,0 +1,173 @@
+import "../App.css";
+import React from "react";
+const First_section = () => {
+  return (
+    <section className="first_section">
+      <div className="section_block">
+        <a href="#s" className="logobox">
+          <img
+            src="https://v.fastcdn.co/u/f91f856b/61499007-0-Meetup20YearsAlt.png"
+            alt=""
+            id="logo"
+          />
+        </a>
+      </div>
+      <div className="years">
+        <h1 className="header_text">
+          20 years of real connections—for{" "}
+          <span id="rotate_word">passions</span> that last
+        </h1>
+        <p className="">
+          <span className="">
+            Your new community is waiting for you. For 20 years, millions of
+            people have chosen Meetup to make real connections over the things
+            that matter. Start a group today.
+          </span>
+        </p>
+      </div>
+      <div className="slide">
+        <img src="o_fcb69dcc2c.jpg" alt="" />
+        {/* <a href="https://www.meetup.com/start?origin=groups">
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/61268591-0-slide-6.jpg"
+            id="slide1"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/61268411-0-slide-2.jpg"
+            id="slide2"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60156740-0-slide-4.jpg"
+            id="slide3"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/61268250-0-slide-8.jpg"
+            id="slide4"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/61268244-0-slide-2.jpg"
+            id="slide5"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/61268246-0-slide-7.jpg"
+            id="slide6"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60156725-0-slide-6.jpg"
+            id="slide7"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60142610-0-slide-1.jpg"
+            id="slide8"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60142600-0-slide-3.jpg"
+            id="slide9"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60141425-0-slide-4.jpg"
+            id="slide10"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60141410-0-slide-6.jpg"
+            id="slide11"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60141395-0-slide-1.jpg"
+            id="slide12"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60139320-0-slide-5.jpg"
+            id="slide13"
+            alt="img"
+          />
+          <img
+            class="mySlides"
+            src="https://v.fastcdn.co/u/f91f856b/60139305-0-slide-1.jpg"
+            id="slide14"
+            alt="img"
+          />
+        </a> */}
+      </div>
+      <div className="meetuporganizing">
+        <h1 className="">Meetup makes organizing easy</h1>
+        <div id="section_orgs">
+          <div className="meetup_orgs">
+            <img
+              src="https://v.fastcdn.co/u/f91f856b/56342857-0-Hola.png"
+              alt=""
+            />
+            <h3>Reach New People</h3>
+            <p>
+              Connect with people in your area who are curious about the things
+              you love.
+            </p>
+            <a href="#s">START NOW</a>
+          </div>
+          <div className="meetup_orgs">
+            <img
+              src="https://v.fastcdn.co/u/f91f856b/56342857-0-Hola.png"
+              alt=""
+            />
+            <h3>Reach New People</h3>
+            <p>
+              Connect with people in your area who are curious about the things
+              you love.
+            </p>
+            <a href="#s">START NOW</a>
+          </div>
+          <div className="meetup_orgs">
+            <img
+              src="https://v.fastcdn.co/u/f91f856b/56342857-0-Hola.png"
+              alt=""
+            />
+            <h3>Reach New People</h3>
+            <p>
+              Connect with people in your area who are curious about the things
+              you love.
+            </p>
+            <a href="#s">START NOW</a>
+          </div>
+          <div className="meetup_orgs">
+            <img
+              src="https://v.fastcdn.co/u/f91f856b/56342857-0-Hola.png"
+              alt=""
+            />
+            <h3>Reach New People</h3>
+            <p>
+              Connect with people in your area who are curious about the things
+              you love.
+            </p>
+            <a href="#s">START NOW</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default First_section;
